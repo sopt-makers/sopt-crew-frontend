3b9416ada918fe00ed2582005d75b7f51cea144d
NaLeum <wognsdl@likelion.org>
2022-10-16 16:01:17 +0900

    chore: box 컴포넌트 추가 및 전체 레이아웃 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Box } from '@components/box/Box';
+import { theme } from 'stitches.config';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Box css={{ maxWidth: '1200px', color: theme.colors.white, mx: '$auto' }}>
+        <Component {...pageProps} />
+      </Box>
     </QueryClientProvider>
   );
 }
