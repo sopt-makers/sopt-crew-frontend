da6d00ca0406fb2fba86019a4fbf6cb74d2a3851
100Gyeon <bjy9966@naver.com>
2022-10-21 01:55:40 +0900

    feat: 모임 상세 페이지에 탭 추가

---
+++ pages/detail/[id].tsx
@@ -0,0 +1,26 @@
+import Tab from '@components/tabs/Tab';
+import Tabs from '@components/tabs/Tabs';
+import { useState } from 'react';
+
+function Detail() {
+  const [selectedTab, setSelectedTab] = useState('introduction');
+  const handleChange = (text: string) => {
+    setSelectedTab(text);
+  };
+
+  return (
+    <div>
+      모임 상세 페이지
+      <Tabs text={selectedTab} onChange={handleChange}>
+        <Tab text={'introduction'}>모임 소개</Tab>
+        <Tab text={'period'}>모임 기간</Tab>
+        <Tab text={'method'}>진행 방식</Tab>
+        <Tab text={'founder'}>개설자 소개</Tab>
+        <Tab text={'target'}>모집 대상</Tab>
+        <Tab text={'note'}>유의 사항</Tab>
+      </Tabs>
+    </div>
+  );
+}
+
+export default Detail;
