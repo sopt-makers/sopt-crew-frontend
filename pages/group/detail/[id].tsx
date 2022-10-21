e450fd8f64823580879a9db14dbb34ed2831192a
100Gyeon <bjy9966@naver.com>
2022-10-22 02:49:50 +0900

    feat: 모임 상세 페이지에서 탭 클릭 시 해당 위치로 이동

--- pages/group/detail/[id].tsx
+++ pages/group/detail/[id].tsx
@@ -1,26 +1,82 @@
+import { Box } from '@components/box/Box';
 import Tab from '@components/tabs/Tab';
 import Tabs from '@components/tabs/Tabs';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { styled } from 'stitches.config';
 
 function Detail() {
-  const [selectedTab, setSelectedTab] = useState('introduction');
+  const tabRef = useRef(new Array());
+  const detailList = [
+    {
+      id: 0,
+      title: '모임 소개',
+      content: '내용',
+    },
+    {
+      id: 1,
+      title: '모임 기간',
+      content: '내용',
+    },
+    {
+      id: 2,
+      title: '진행 방식',
+      content: '내용',
+    },
+    {
+      id: 3,
+      title: '개설자 소개',
+      content: '내용',
+    },
+    {
+      id: 4,
+      title: '모집 대상',
+      content: '내용',
+    },
+    {
+      id: 5,
+      title: '유의사항',
+      content: '내용',
+    },
+  ];
+  const [selectedTab, setSelectedTab] = useState(detailList[0].title);
+
   const handleChange = (text: string) => {
     setSelectedTab(text);
+    tabRef.current[
+      detailList.findIndex(item => item.title === text)
+    ].scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <div>
-      모임 상세 페이지
       <Tabs text={selectedTab} size={'small'} onChange={handleChange}>
-        <Tab text={'introduction'}>모임 소개</Tab>
-        <Tab text={'period'}>모임 기간</Tab>
-        <Tab text={'method'}>진행 방식</Tab>
-        <Tab text={'founder'}>개설자 소개</Tab>
-        <Tab text={'target'}>모집 대상</Tab>
-        <Tab text={'note'}>유의 사항</Tab>
+        {detailList.map(({ title }) => (
+          <Tab text={title}>{title}</Tab>
+        ))}
       </Tabs>
+      {detailList.map(({ id, title, content }) => (
+        <SDetail ref={element => (tabRef.current[id] = element)}>
+          <STitle>{title}</STitle>
+          <SContent>{content}</SContent>
+        </SDetail>
+      ))}
     </div>
   );
 }
 
 export default Detail;
+
+const SDetail = styled(Box, {
+  color: '$white',
+  marginTop: '$120',
+});
+
+const STitle = styled(Box, {
+  fontAg: '24_bold_100',
+  marginBottom: '$24',
+});
+
+const SContent = styled(Box, {
+  fontSize: '$22',
+  lineHeight: '37.4px',
+});
