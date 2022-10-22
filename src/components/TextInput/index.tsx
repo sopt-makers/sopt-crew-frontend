d1b63b6d0f528004a8c23a7e3cdb59ed348fc276
eunsukimme <eunsu.dev@gmail.com>
2022-10-22 17:29:37 +0900

    feat: add styling

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,22 +1,49 @@
+import { styled } from '@stitches/react';
 import React from 'react';
 
 type TextInputProps = {
   label: string;
-  message: string;
-  required: boolean;
+  message?: string;
+  required?: boolean;
 };
 
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
   ({ label, message, required }: TextInputProps, ref) => (
     <>
-      <label>
+      <Label>
         {label}
         {required ? '*' : ''}
-      </label>
-      <span>{message}</span>
-      <input type="text" ref={ref} />
+      </Label>
+      {message && <Message>{message}</Message>}
+      <Input type="text" ref={ref} />
     </>
   )
 );
+
+const Label = styled('label', {
+  marginBottom: 12,
+  display: 'inline-block',
+  width: '100%',
+  fontAg: '18_semibd_100',
+  color: '$white',
+});
+const Message = styled('span', {
+  marginBottom: 18,
+  display: 'inline-block',
+  fontAg: '14_medium_100',
+  color: '$gray100',
+});
+const Input = styled('input', {
+  padding: '18px 20px',
+  display: 'flex',
+  alignItems: 'center',
+  fontAg: '16_medium_100',
+  color: '$white',
+  background: '$black60',
+  borderRadius: 10,
+  '&::placeholder': {
+    color: '$gray100',
+  },
+});
 
 export default TextInput;
