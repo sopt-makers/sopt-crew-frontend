71b7a6ad3fa3a5a5e51478ab68f00d888204bb2e
eunsukimme <eunsu.dev@gmail.com>
2022-11-05 17:55:16 +0900

    style: edit Select button style

--- src/components/Form/Select/Button/index.tsx
+++ src/components/Form/Select/Button/index.tsx
@@ -33,7 +33,7 @@ const SButton = styled('button', {
   gap: 8,
   fontAg: '16_medium_100',
   color: '$white',
-  background: '$black40',
+  background: '$black60',
   borderRadius: 10,
   variants: {
     isNotSelected: {
