a1d94d78a887db9d624c0320097d117d784e292b
eunsukimme <eunsu.dev@gmail.com>
2022-11-05 16:48:34 +0900

    chore: add data and types

---
+++ src/types/form.ts
@@ -0,0 +1,14 @@
+export interface FormData {
+  title: string;
+  category: string;
+  image: string;
+  applicationStartAt: string;
+  applicationEndAt: string;
+  memberCount: number;
+  detailIntro: string;
+  detailFlow: string;
+  startAt: string;
+  endAt: string;
+  detailCreator: string;
+  detailTargetMember: string;
+}
