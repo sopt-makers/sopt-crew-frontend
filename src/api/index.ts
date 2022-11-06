2775f46fc1bd1964a63a4ea8f2086117573199fe
eunsukimme <eunsu.dev@gmail.com>
2022-11-07 00:39:16 +0900

    feat: add create group api

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// TODO: change after deploy server
+const baseURL = 'https://makers-web.herokuapp.com';
+
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+});
