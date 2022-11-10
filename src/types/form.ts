1dd8857f6adc486a4a3365177562f5d1ec5d9253
eunsukimme <eunsu.dev@gmail.com>
2022-11-11 02:13:31 +0900

    feat: add files field in zod schema

--- src/types/form.ts
+++ src/types/form.ts
@@ -6,8 +6,17 @@ export const schema = z.object({
     label: z.string(),
     value: z.string(),
   }),
-  // TODO: add iamge
-  // image: z.string(),
+  files: z
+    .any()
+    .refine((files: FileList) => files?.length > 0, '이미지를 추가해주세요.')
+    .refine(
+      (files: FileList) => files?.[0]?.size <= MAX_FILE_SIZE,
+      `5MB 이하의 사진만 업로드할 수 있습니다.`
+    )
+    .refine(
+      (files: FileList) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      '.jpg, .jpeg, .png, .webp, .gif 파일만 업로드할 수 있습니다.'
+    ),
   startDate: z.string().min(1),
   endDate: z.string().min(1),
   capacity: z.number().gt(0),
@@ -23,3 +32,13 @@ export const schema = z.object({
 });
 
 export type FormType = z.infer<typeof schema>;
+
+const MAX_FILE_SIZE = 500 * 1000; // 5MB
+
+export const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+  'image/gif',
+];
