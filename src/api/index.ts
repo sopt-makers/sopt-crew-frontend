aa6bfbe3706334656b98b4624bb56cec434fc900
NaLeum <wognsdl@likelion.org>
2022-11-10 17:05:48 +0900

    feat: axiosWithAuth 임시제작

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,20 @@
 import axios from 'axios';
 
+export type PromiseResponse<T> = { data: T; statusCode: number };
+
 // TODO: change after deploy server
 const baseURL = 'https://makers-web.herokuapp.com';
 
 export const api = axios.create({
   baseURL,
   // withCredentials: true,
+});
+
+// accessToken 키가 필요한 요청이 있어서 임시 제작
+export const apiWithAuth = axios.create({
+  baseURL,
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibGVlIiwiaWQiOjIsImlhdCI6MTY2Nzc0MTgxNywiZXhwIjoxNzAzNzQxODE3fQ.itd4icu8KBGjjkIL4RgcDZFFykUo66pxWjrKGUB3ZOE',
+  },
 });
