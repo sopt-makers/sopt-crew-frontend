d90b2bb065d79681769a55220d98808770369475
100Gyeon <bjy9966@naver.com>
2022-11-13 02:26:20 +0900

    chore: Headless UI에서 제공하는 Dialog 적용하면서 불필요해진 부분 제거

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function useModal() {
   const [isModalOpened, setIsModalOpened] = useState(false);
@@ -10,19 +10,6 @@ export default function useModal() {
   const handleModalClose = () => {
     setIsModalOpened(false);
   };
-
-  useEffect(() => {
-    if (isModalOpened) {
-      document.body.style.overflowY = 'hidden';
-      return;
-    }
-
-    document.body.style.overflowY = 'auto';
-
-    return () => {
-      document.body.style.overflowY = 'auto';
-    };
-  }, [isModalOpened]);
 
   return {
     isModalOpened,
