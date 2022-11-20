123bf054e3b7a4dbeb91908dfcdd96315b6a6af2
eunsukimme <eunsu.dev@gmail.com>
2022-11-21 01:03:47 +0900

    feat: add ErrorMessage

---
+++ src/components/Form/ErrorMessage/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from 'stitches.config';
+
+const ErrorMessage = styled('span', {
+  display: 'inline-block',
+  fontAg: '12_medium_100',
+  color: '$red100',
+});
+
+export default ErrorMessage;
