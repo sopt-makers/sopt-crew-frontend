5641817548d092fe9fa43399f0effe422d44dd9e
eunsukimme <eunsu.dev@gmail.com>
2022-12-18 14:34:51 +0900

    feat: integrate auth hook with app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,26 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Box } from '@components/box/Box';
 import { theme } from 'stitches.config';
 import Header from '@components/header/Header';
+import { useRouter } from 'next/router';
+import useAuth from '@hooks/useAuth';
+import { useEffect } from 'react';
+import { api } from 'src/api';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { accessToken } = useAuth();
+
+  useEffect(() => {
+    if (!accessToken) {
+      localStorage.setItem('lastUnauthorizedPath', window.location.pathname);
+      router.push(`/auth/login`);
+      return;
+    }
+    // set access token in header
+    api.defaults.headers.common['Authorization'] = accessToken;
+  }, [router, accessToken]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Box
