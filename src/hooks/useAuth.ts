fa55fd161ed13c9f81282fadb5c2380549a01ce9
eunsukimme <eunsu.dev@gmail.com>
2022-12-18 14:57:56 +0900

    feat: add logout

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -5,6 +5,11 @@ const ACCESS_TOKEN_KEY = 'serviceAccessToken';
 export default function useAuth() {
   const [accessToken, setAccessToken] = useState<string | null>('');
 
+  const logout = () => {
+    localStorage.removeItem(ACCESS_TOKEN_KEY);
+    window.location.pathname = '/auth/login';
+  };
+
   useEffect(() => {
     // TODO: remove after test
     if (process.env.NODE_ENV === 'development') {
@@ -12,11 +17,12 @@ export default function useAuth() {
         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibGVlIiwidXNlcklkIjoxLCJpYXQiOjE2Njg0MzM0MTMsImV4cCI6MTcwNDQzMzQxM30.NGbf96zcykC0QQERvSe5F5S2uZO8Tuc13mkpb73y2Bo'
       );
     } else {
-    setAccessToken(localStorage.getItem(ACCESS_TOKEN_KEY));
+      setAccessToken(localStorage.getItem(ACCESS_TOKEN_KEY));
     }
   }, []);
 
   return {
     accessToken,
+    logout,
   };
 }
