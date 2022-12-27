f178cf9865e87bed46dd8140620875dac7a982d1
NaLeum <wognsdl@likelion.org>
2022-12-27 19:24:59 +0900

    feat: add logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -15,9 +15,10 @@ import { Box } from '@components/box/Box';
 import { useGetMemberOfMe } from 'src/api/members/hooks';
 import { FEEDBACK_FORM_URL } from '@constants/link';
 import { usePlaygroundLink } from '@hooks/usePlaygroundLink';
+import useAuth from '@hooks/useAuth';
 
 const Header: FC = () => {
-  //   const { logout } = useAuth();
+  const { logout } = useAuth();
   const { pathname, events, basePath } = useRouter();
   const [isUserDropdownOpened, setIsUserDropdownOpened] = useState(false);
   const [isMobileMenuOpened, setIsMobileMenuOpened] = useState(false);
@@ -130,7 +131,7 @@ const Header: FC = () => {
         >
           내 프로필
         </Link>
-        <div onClick={() => 'logout()'}>로그아웃</div>
+        <div onClick={logout}>로그아웃</div>
       </UserDropdown>
 
       <DimmedBackground
@@ -178,7 +179,7 @@ const Header: FC = () => {
           <MenuLink href={FEEDBACK_FORM_URL} target="_blank">
             의견 제안하기
           </MenuLink>
-          <MenuLink onClick={() => 'logout()'}>로그아웃</MenuLink>
+          <MenuLink onClick={logout}>로그아웃</MenuLink>
         </MenuWrapper>
       </MobileMenu>
     </StyledHeader>
