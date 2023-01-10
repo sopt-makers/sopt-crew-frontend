14794747efb30abd0eca6b7eb9c7ee653fc39c65
NaLeum <wognsdl@likelion.org>
2023-01-10 22:45:33 +0900

    feat: Apply type 추가

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,5 +1,6 @@
 export const RECRUITMENT_STATUS = ['모집 전', '모집 중', '모집 마감'];
 export const APPLY_STATUS = ['대기', '승인', '거절'];
+export const APPLY_TYPE = ['신청', '초대'];
 export enum EApplyStatus {
   'WAITING',
   'APPROVE',
