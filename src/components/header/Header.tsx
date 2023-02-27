7d59888dd9fdb9143bf4adaf0957a0525581dfa9
100Gyeon <bjy9966@naver.com>
2023-02-27 23:12:26 +0900

    chore: 임시 type 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import { styled } from 'stitches.config';
 import { Box } from '@components/box/Box';
 import useAuth from '@hooks/useAuth';
 import { useGetMemberOfMe } from 'src/api/members/hooks';
-import { DesktopHeader, MobileHeader, playgroundLink, LinkRenderer } from '@sopt-makers/playground-common';
+import { DesktopHeader, MobileHeader, playgroundLink } from '@sopt-makers/playground-common';
+
+// TODO: playground 팀에서 type export하면 제거할 예정
+type LinkRendererParams = {
+  href: string;
+  children: ReactNode;
+};
 
 const Header: FC = () => {
   const { logout } = useAuth();
   const { data: me } = useGetMemberOfMe();
   const user = me ? { id: `${me.id}`, name: me.name, image: me.profileImage ?? undefined } : null;
 
-  const renderLink: LinkRenderer = ({ href, children }) => {
+  const renderLink = ({ href, children }: LinkRendererParams) => {
     if (href.startsWith(playgroundLink.groupList())) {
       return (
         <Link href="/" passHref>
