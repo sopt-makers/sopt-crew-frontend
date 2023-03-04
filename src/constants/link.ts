8ed35e732f2f91745ac590c34414b1a35a3495c1
100Gyeon <bjy9966@naver.com>
2023-03-04 19:44:45 +0900

    chore: 플그팀 헤더 적용으로 필요 없어진 링크 제거

--- src/constants/link.ts
+++
@@ -1,3 +0,0 @@
-export const FEEDBACK_FORM_URL = 'https://forms.gle/FCx5WJ6mDmRuneQi9';
-export const NOTIFY_2ND_GENERATION_URL = 'https://forms.gle/AKf164VXtJPGJRoo8';
-export const MEMBER_REQUEST_FORM_URL = 'https://forms.gle/Hs9tJgMG9bNvT1rS9';
