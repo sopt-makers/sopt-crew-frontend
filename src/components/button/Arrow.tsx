c6d02b959456bff97d2a514ee35fedf7dde7eb73
NaLeum <wognsdl@likelion.org>
2023-03-10 10:47:33 +0900

    chore: 잘못 수정된 부분 원상복구

--- src/components/button/Arrow.tsx
+++ src/components/button/Arrow.tsx
@@ -3,10 +3,9 @@ import ArrowBigRight from '@assets/svg/arrow_big_right.svg';
 import ArrowSmallRight from '@assets/svg/arrow_small_right.svg';
 import { MouseEventHandler } from 'react';
 import { styled } from '@stitches/react';
-import { Box } from '@components/box/Box';
 
 interface ArrowButtonProps {
-  onClick?: MouseEventHandler<HTMLDivElement>;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   css?: CSSType;
   direction: 'right' | 'top' | 'left' | 'bottom';
   disabled?: boolean;
@@ -22,14 +21,14 @@ const ArrowButton = ({
   ...props
 }: ArrowButtonProps) => {
   return (
-    <SWrapper css={{ ...css }} disabled={disabled} onClick={onClick} direction={direction} {...props}>
+    <SButton css={{ ...css }} disabled={disabled} onClick={onClick} direction={direction} {...props}>
       {size === 'big' && <ArrowBigRight />}
       {size === 'small' && <ArrowSmallRight />}
-    </SWrapper>
+    </SButton>
   );
 };
 
-const SWrapper = styled(Box, {
+const SButton = styled('button', {
   p: '$8',
   '& svg': {
     display: 'block',
