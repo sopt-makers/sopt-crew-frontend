d9d64f1c046c2378eb5e8ea1ce3885e8e98d9ecd
100Gyeon <bjy9966@naver.com>
2023-03-17 17:43:55 +0900

    chore: SelectButton 스타일링 수정

--- src/components/form/Select/Button/index.tsx
+++ src/components/form/Select/Button/index.tsx
@@ -26,12 +26,12 @@ export default Button;
 
 const SButton = styled('button', {
   minWidth: '147px',
-  padding: '16px 20px 16px 16px',
+  padding: '$18 $20',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
   gap: 8,
-  fontAg: '16_medium_100',
+  fontAg: '18_medium_100',
   color: '$white',
   background: '$black60',
   borderRadius: 10,
