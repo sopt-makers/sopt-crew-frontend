b8fc03138a079fbcc1e8bd09aebc14cc68d83f07
eunsukimme <eunsu.dev@gmail.com>
2023-03-19 15:28:19 +0900

    chore: fix typo

--- src/components/form/Select/utils/index.ts
+++ src/components/form/Select/utils/index.ts
@@ -2,4 +2,4 @@ import type { Option } from '../OptionItem';
 
 export const isAllSelect = (option?: Option) => option?.value === 'all';
 export const isAllUnselected = (prev: Option[], next: Option[]) =>
-  prev.some(v => v.value === 'all' && !next.some(v => v.value === 'all'));
+  prev.some(v => v.value === 'all') && !next.some(v => v.value === 'all');
