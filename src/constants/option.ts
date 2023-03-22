6b7ec443620773acc1443219f47c5cde360551a5
100Gyeon <bjy9966@naver.com>
2023-03-22 12:42:11 +0900

    chore: 텍스트 수정

--- src/constants/option.ts
+++ src/constants/option.ts
@@ -11,7 +11,7 @@ export const RECRUITMENT_STATUS = ['모집 전', '모집 중', '모집 마감'];
 export const APPROVAL_STATUS = ['대기', '승인', '거절'];
 export const APPLICATION_TYPE = ['신청', '초대'];
 export const CATEGORY_OPTIONS = ['스터디'];
-export const PART_OPTIONS = ['기획', '디자인', '안드로이드', 'IOS', '웹', '서버'];
+export const PART_OPTIONS = ['기획', '디자인', '안드로이드', 'iOS', '웹', '서버'];
 export const PART_VALUES = ['PM', 'DESIGN', 'ANDROID', 'IOS', 'WEB', 'SERVER'];
 
 export const enum ERecruitmentStatus {
