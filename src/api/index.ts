2afe6e650a3275b6e6c0fcd1fad14fb5d8dda4d2
100Gyeon <bjy9966@naver.com>
2023-03-23 18:54:59 +0900

    fix: env 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,10 @@ export type PromiseResponse<T> = { data: T; statusCode: number };
 export type Data<T> = PromiseResponse<T>;
 
 const baseURL =
-  process.env.APP_ENV === 'production' ? 'https://crew.api.prod.sopt.org' : 'https://crew.api.dev.sopt.org';
+  process.env.NODE_ENV === 'production' ? 'https://crew.api.prod.sopt.org' : 'https://crew.api.dev.sopt.org';
 
 const playgroundBaseURL =
-  process.env.APP_ENV === 'production' ? 'https://playground.api.sopt.org/' : 'https://playground.dev.sopt.org/';
+  process.env.NODE_ENV === 'production' ? 'https://playground.api.sopt.org/' : 'https://playground.dev.sopt.org/';
 
 export const api = axios.create({
   baseURL,
