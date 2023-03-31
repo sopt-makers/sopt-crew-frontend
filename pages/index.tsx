66b7255617d87d3a2738c3b3ee557143a90e3a43
100Gyeon <bjy9966@naver.com>
2023-03-31 16:13:08 +0900

    chore: 모임 개설과 관련된 부분 주석 처리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import { styled } from 'stitches.config';
-import { useGetMemberOfMe } from 'src/api/members/hooks';
-import useModal from '@hooks/useModal';
-import moveToProfileUploadPage from '@utils/moveToProfileUploadPage';
-import ConfirmModal from '@components/modal/ConfirmModal';
+// import { useGetMemberOfMe } from 'src/api/members/hooks';
+// import useModal from '@hooks/useModal';
+// import moveToProfileUploadPage from '@utils/moveToProfileUploadPage';
+// import ConfirmModal from '@components/modal/ConfirmModal';
 import { Box } from '@components/box/Box';
 import { TabList } from '@components/tabList/TabList';
 import { Flex } from '@components/util/layout/Flex';
@@ -15,21 +15,21 @@ import Filter from '@components/page/meetingList/Filter';
 import Search from '@components/page/meetingList/Filter/Search';
 import GridLayout from '@components/page/meetingList/Grid/Layout';
 import CardSkeleton from '@components/page/meetingList/Card/Skeleton';
-import PlusIcon from '@assets/svg/plus.svg';
-import WriteIcon from '@assets/svg/write.svg';
+// import PlusIcon from '@assets/svg/plus.svg';
+// import WriteIcon from '@assets/svg/write.svg';
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  const { data: me } = useGetMemberOfMe();
-  const { isModalOpened, handleModalOpen, handleModalClose } = useModal();
+  // const router = useRouter();
+  // const { data: me } = useGetMemberOfMe();
+  // const { isModalOpened, handleModalOpen, handleModalClose } = useModal();
 
-  const handleMakeMeeting = () => {
-    if (!me?.hasProfile) {
-      handleModalOpen();
-      return;
-    }
-    router.push('/make');
-  };
+  // const handleMakeMeeting = () => {
+  //   if (!me?.hasProfile) {
+  //     handleModalOpen();
+  //     return;
+  //   }
+  //   router.push('/make');
+  // };
 
   return (
     <>
@@ -48,13 +48,13 @@ const Home: NextPage = () => {
             </Link>
           </TabList>
           <SMobileButtonContainer>
-            <WriteIcon onClick={handleMakeMeeting} className="make-button" />
+            {/* <WriteIcon onClick={handleMakeMeeting} className="make-button" /> */}
             <Search.Mobile />
           </SMobileButtonContainer>
-          <SMakeMeetingButton onClick={handleMakeMeeting}>
+          {/* <SMakeMeetingButton onClick={handleMakeMeeting}>
             <PlusIcon />
             <span>모임 개설하기</span>
-          </SMakeMeetingButton>
+          </SMakeMeetingButton> */}
         </Flex>
         <SFilterWrapper>
           <Filter />
@@ -71,34 +71,34 @@ const Home: NextPage = () => {
           <MeetingListOfAll />
         </SSRSafeSuspense>
       </div>
-      <ConfirmModal
+      {/* <ConfirmModal
         isModalOpened={isModalOpened}
         message={`모임을 개설하려면\n프로필 작성이 필요해요`}
         cancelButton="돌아가기"
         confirmButton="작성하기"
         handleModalClose={handleModalClose}
         handleConfirm={moveToProfileUploadPage}
-      />
+      /> */}
     </>
   );
 };
 
 export default Home;
 
-const SMakeMeetingButton = styled('button', {
-  flexType: 'verticalCenter',
-  padding: '$18 $24 $18 $20',
-  background: '$purple100',
-  borderRadius: '12px',
-  '& > span': {
-    ml: '$12',
-    fontAg: '18_bold_100',
-    color: '$white',
-  },
-  '@mobile': {
-    display: 'none',
-  },
-});
+// const SMakeMeetingButton = styled('button', {
+//   flexType: 'verticalCenter',
+//   padding: '$18 $24 $18 $20',
+//   background: '$purple100',
+//   borderRadius: '12px',
+//   '& > span': {
+//     ml: '$12',
+//     fontAg: '18_bold_100',
+//     color: '$white',
+//   },
+//   '@mobile': {
+//     display: 'none',
+//   },
+// });
 
 const SMobileButtonContainer = styled(Box, {
   display: 'none',
