fb25a73b47af2f25b42925aa6cae2538d579a28c
eunsukimme <eunsu.dev@gmail.com>
2023-05-06 23:27:25 +0900

    chore: add environment options on sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,5 +2,6 @@ import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.APP_ENV === 'production' ? 'production' : 'development',
   tracesSampleRate: 1.0,
 });
