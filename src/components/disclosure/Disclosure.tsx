a6bd8c69271ebfed0e1ded16088f3470491c5401
eunsukimme <eunsu.dev@gmail.com>
2023-05-28 01:48:32 +0900

    feat: add responsive styles

--- src/components/disclosure/Disclosure.tsx
+++ src/components/disclosure/Disclosure.tsx
@@ -42,12 +42,19 @@ const Container = styled('div', {
   padding: '48px',
   background: '$black80',
   borderRadius: '20px',
+  '@mobile': {
+    padding: '24px 16px',
+    borderRadius: '12px',
+  },
 });
 const STitleContainer = styled(Flex, {
   width: '100%',
 });
 const STitleWrapper = styled(Flex, {
   gap: '14px',
+  '@mobile': {
+    gap: '6px',
+  },
 });
 const STitle = styled('h1', {
   /* Crew/Headline/1 */
@@ -55,6 +62,11 @@ const STitle = styled('h1', {
   fontSize: '24px',
   lineHeight: '24px',
   color: '$white',
+  '@mobile': {
+    /* Crew/Headline/5 */
+    fontSize: '14px',
+    lineHeight: '14px',
+  },
 });
 const SSubTitle = styled('h4', {
   /* Crew/Title/4 */
@@ -62,8 +74,16 @@ const SSubTitle = styled('h4', {
   fontSize: '16px',
   lineheight: '24px',
   color: 'gray60',
+  '@mobile': {
+    /* Crew/Body/4 */
+    fontWeight: '500',
+    fontSize: '12px',
+    lineHeight: '18px',
+  },
 });
 const SArrowIcon = styled(ArrowIcon, {
+  width: '32px',
+  height: '32px',
   color: '$white',
   variants: {
     isOpen: {
@@ -72,22 +92,37 @@ const SArrowIcon = styled(ArrowIcon, {
       },
     },
   },
+  '@mobile': {
+    width: '24px',
+    height: '24px',
+  },
 });
 const SDivider = styled('div', {
   height: '1px',
   margin: '36px 0 32px 0',
   background: '$black40',
+  '@mobile': {
+    margin: '20px 0',
+  },
 });
 const SContents = styled('div', {
-  padding: '0 9px',
+  padding: '0 4px',
   /* Crew/Body/2 */
   fontWeight: '500',
   fontSize: '16px',
   lineHeight: '26px',
   color: '$gray30',
+  '@mobile': {
+    /* Crew/Body/4 */
+    fontSize: '12px',
+    lineHeight: '18px',
+  },
 });
 const SInfo = styled(Flex, {
   gap: '6px',
+  '@mobile': {
+    marginTop: '14px',
+  },
 });
 const SCreatedAt = styled('span', {
   /* Crew/Title/6 */
@@ -95,6 +130,12 @@ const SCreatedAt = styled('span', {
   fontSize: '12px',
   lineheight: '18px',
   color: '$gray100',
+  '@mobile': {
+    /* Crew/Caption/1 */
+    fontWeight: '700',
+    fontSize: '10px',
+    lineHeight: '10px',
+  },
 });
 const SDate = styled('span', {
   /* Crew/Body/4 */
@@ -102,4 +143,10 @@ const SDate = styled('span', {
   fontSize: '12px',
   lineHeight: '18px',
   color: '$gray60',
+  '@mobile': {
+    /* Crew/Caption/1 */
+    fontWeight: '700',
+    fontSize: '10px',
+    lineHeight: '10px',
+  },
 });
