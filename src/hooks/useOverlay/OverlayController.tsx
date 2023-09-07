b4c39a465e3d8e3b820f12cbc9492d20845d0316
na-reum <devnareum@gmail.com>
2023-09-07 21:03:41 +0900

    feat: useOverlay 추가

---
+++ src/hooks/useOverlay/OverlayController.tsx
@@ -0,0 +1,37 @@
+import { Ref, useImperativeHandle, forwardRef, useEffect, useState, useCallback } from 'react';
+
+import { CreateOverlayElement } from './types';
+
+interface Props {
+  overlayElement: CreateOverlayElement;
+  onExit: () => void;
+}
+
+export interface OverlayControlRef {
+  close: () => void;
+}
+
+export const OverlayController = forwardRef(function OverlayController(
+  { overlayElement: OverlayElement, onExit }: Props,
+  ref: Ref<OverlayControlRef>
+) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = useCallback(() => setIsOpen(false), []);
+
+  useImperativeHandle(
+    ref,
+    () => {
+      return { close: handleClose };
+    },
+    [handleClose]
+  );
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      setIsOpen(true);
+    });
+  }, []);
+
+  return <OverlayElement isOpen={isOpen} close={handleClose} exit={onExit} />;
+});
