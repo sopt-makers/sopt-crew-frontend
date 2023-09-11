1986f30cc5a3efdcbee2703d7533243ffa4da379
Eunsu(Evan) Kim <eunsu.dev@gmail.com>
2023-09-11 23:26:52 +0900

    Avatar 컴포넌트 Overlay 인터페이스 추가 (#385)

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -5,17 +5,20 @@ interface AvatarProps {
   src: string;
   alt: string;
   sx?: CSSProperties;
+  Overlay?: React.ReactNode;
 }
 
-export default function Avatar({ src, alt, sx }: AvatarProps) {
+export default function Avatar({ src, alt, sx, Overlay }: AvatarProps) {
   return (
     <SContainer style={sx}>
+      {Overlay}
       <SImage src={src} alt={alt} />
     </SContainer>
   );
 }
 
 const SContainer = styled('div', {
+  position: 'relative',
   width: '32px',
   height: '32px',
   borderRadius: '50%',
