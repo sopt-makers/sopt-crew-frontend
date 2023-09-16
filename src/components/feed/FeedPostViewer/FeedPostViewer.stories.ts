5d6a3c28f9db530f75ef28276a4edbe0f724b2fb
eunsukimme <eunsu.dev@gmail.com>
2023-09-17 00:22:05 +0900

    chore: edit FeedPostViewer story to pass multiple images

--- src/components/feed/FeedPostViewer/FeedPostViewer.stories.ts
+++ src/components/feed/FeedPostViewer/FeedPostViewer.stories.ts
@@ -20,7 +20,12 @@ export const Default: Story = {
       title: '하트시그널 시즌 1부터 시즌 4까지 중에 가장 인기가 많았던 출연자는?',
       contents: `1번 김현우\n2번 오영주\n3번 임현주\n4번 박지현\n5번 기타`,
       updatedDate: '2시간 전',
-      images: ['https://via.placeholder.com/600/92c952'],
+      images: [
+        'https://via.placeholder.com/600/92c952',
+        'https://via.placeholder.com/600/771796',
+        'https://via.placeholder.com/600/92c952',
+        'https://via.placeholder.com/600/771796',
+      ],
       user: {
         id: 1,
         name: '김지민',
