87f97d877d5dce965317e4a830655b623f30b7d6
100Gyeon <bjy9966@naver.com>
2023-09-17 15:37:09 +0900

    feat: process.env 활용하는 방식으로 변경

--- src/hooks/amplitude/useAmplitudeInstance.ts
+++ src/hooks/amplitude/useAmplitudeInstance.ts
@@ -3,12 +3,11 @@ import * as amplitude from '@amplitude/analytics-browser';
 
 export default function useAmplitudeInstance() {
   useEffect(() => {
-    const API_KEY =
-      process.env.NEXT_PUBLIC_APP_ENV === 'production'
-        ? '765c1919975291c332c4fd993d9b9a9d'
-        : '94650b2a33ffa5e8cc81b4ec3880730d';
-    amplitude.init(API_KEY);
-    const amplitudeObj = new amplitude.Identify();
-    amplitude.identify(amplitudeObj);
+    const API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
+    if (API_KEY) {
+      amplitude.init(API_KEY);
+      const amplitudeObj = new amplitude.Identify();
+      amplitude.identify(amplitudeObj);
+    }
   }, []);
 }
