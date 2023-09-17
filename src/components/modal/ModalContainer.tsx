47eb5b0b9d2ed8ff94ef1c9528df4700026cb2b5
na-reum <devnareum@gmail.com>
2023-09-17 22:54:57 +0900

    feat: ModalBackground props 타입 제공으로 인한 재사용성 증가

--- src/components/modal/ModalContainer.tsx
+++ src/components/modal/ModalContainer.tsx
@@ -33,7 +33,7 @@ const ModalContainer = ({
       fallback={
         <Show when={isModalOpened}>
           <div>
-            <SModalBackground
+            <ModalBackground
               onClick={e => {
                 e.stopPropagation();
                 handleModalClose();
@@ -56,15 +56,4 @@ export default ModalContainer;
 
 const ModalContent = styled('div', {
   zIndex: 100,
-});
-
-const SModalBackground = styled(Box, {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  zIndex: '$2',
-  width: '100%',
-  height: '100%',
-  backgroundColor: '$black80_trans',
 });
