570dcdef9a4e43f071431e45f7e4aabbd1c91fd1
Jiyeon Baek <58380158+100Gyeon@users.noreply.github.com>
2023-09-17 20:58:41 +0900

    fix: group 추가 (#426)

--- src/components/seo/SEO.tsx
+++ src/components/seo/SEO.tsx
@@ -9,14 +9,14 @@ const SEO = () => {
       <meta property="og:site_name" content="SOPT Playground" />
       <meta property="og:title" content="SOPT ㅣ 모임 신청·개설하기" />
       <meta property="og:description" content="솝트와 더 친해지고 싶으신가요?" />
-      <meta property="og:image" content="/assets/images/thumbnail.jpg" />
+      <meta property="og:image" content="/group/assets/images/thumbnail.jpg" />
       <meta property="og:url" content="https://playground.sopt.org/group" />
       <meta property="og:locale" content="ko_KR" />
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:site" content="SOPT Playground" />
       <meta property="twitter:title" content="SOPT ㅣ 모임 신청·개설하기" />
       <meta property="twitter:description" content="솝트와 더 친해지고 싶으신가요?" />
-      <meta property="twitter:image" content="/assets/images/thumbnail.jpg" />
+      <meta property="twitter:image" content="/group/assets/images/thumbnail.jpg" />
       <meta property="twitter:url" content="https://playground.sopt.org/group" />
     </Head>
   );
