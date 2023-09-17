3e290a6d13c3bf00c24b68fdb198f10b77b0cbec
Eunsu(Evan) Kim <eunsu.dev@gmail.com>
2023-09-17 17:37:01 +0900

    피드 상세 뷰 게시글 조회 API 연동 (#420)
    chore: integrate route query

---
+++ pages/post/index.tsx
@@ -0,0 +1,29 @@
+import { paths } from '@/__generated__/schema';
+import { apiV2 } from '@api/index';
+import FeedPostViewer from '@components/feed/FeedPostViewer/FeedPostViewer';
+import Loader from '@components/loader/Loader';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+
+export default function PostPage() {
+  const { query } = useRouter();
+  const data = useQuery({
+    queryFn: () => apiV2.GET('/post/v1/{postId}', { params: { path: { postId: Number(query.id as string) } } }),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    select: res => res.data.data,
+    enabled: !!query.id,
+  });
+
+  // TODO: 자동으로 타입 추론 되게끔 endpoint 수정 필요
+  const post = data.data as paths['/post/v1/{postId}']['get']['responses']['200']['content']['application/json'];
+
+  // TODO: loading 스켈레톤 UI가 있으면 좋을 듯
+  if (!post) return <Loader />;
+
+  return (
+    <div>
+      <FeedPostViewer post={post} Actions={['수정', '삭제']} />
+    </div>
+  );
+}
