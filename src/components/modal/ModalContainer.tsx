06f4007e960cc8f8ca37f4a28b90989d3a23a8c9
na-reum <devnareum@gmail.com>
2023-09-23 18:36:19 +0900

    feat: useOverlay 를 걷어내고 Nest구조로 변경

--- src/components/modal/ModalContainer.tsx
+++ src/components/modal/ModalContainer.tsx
@@ -1,48 +1,19 @@
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren } from 'react';
 import ModalBackground from './ModalBackground';
 import { Dialog } from '@headlessui/react';
-import { styled } from 'stitches.config';
-import Show from '@components/util/Show';
 
 export interface ModalContainerProps {
   isModalOpened: boolean;
   handleModalClose: () => void;
-  isHeadlessUi?: boolean;
 }
 
-const ModalContainer = ({
-  isModalOpened,
-  handleModalClose,
-  isHeadlessUi = true,
-  children,
-}: PropsWithChildren<ModalContainerProps>) => {
+const ModalContainer = ({ isModalOpened, handleModalClose, children }: PropsWithChildren<ModalContainerProps>) => {
   return (
-    <Show
-      when={isHeadlessUi}
-      fallback={
-        <Show when={isModalOpened}>
-          <div>
-            <ModalBackground
-              onClick={e => {
-                e.stopPropagation();
-                handleModalClose();
-              }}
-            />
-            <ModalContent>{children}</ModalContent>
-          </div>
-        </Show>
-      }
-    >
-      <Dialog open={isModalOpened} onClose={handleModalClose}>
-        <ModalBackground />
-        <Dialog.Panel>{children}</Dialog.Panel>
-      </Dialog>
-    </Show>
+    <Dialog open={isModalOpened} onClose={handleModalClose}>
+      <ModalBackground />
+      <Dialog.Panel>{children}</Dialog.Panel>
+    </Dialog>
   );
 };
 
 export default ModalContainer;
-
-const ModalContent = styled('div', {
-  zIndex: 100,
-});
