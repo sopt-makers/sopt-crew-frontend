ce37dc7ad6b95c941b67d897f0f9a207abee4d7d
100Gyeon <bjy9966@naver.com>
2023-09-25 01:55:09 +0900

    chore: 공식문서처럼 수정

--- src/api/post/hooks.ts
+++ src/api/post/hooks.ts
@@ -4,7 +4,7 @@ import { getInfinitePosts } from '.';
 export const useInfinitePosts = (take: number, meetingId: number) => {
   const { data, hasNextPage, fetchNextPage } = useInfiniteQuery({
     queryKey: ['getInfinitePosts', take, meetingId],
-    queryFn: ({ pageParam }) => getInfinitePosts(pageParam ?? 1, take, meetingId),
+    queryFn: ({ pageParam = 1 }) => getInfinitePosts(pageParam, take, meetingId),
     getNextPageParam: (lastPage, allPages) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
