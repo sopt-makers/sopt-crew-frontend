d7fe66cfb9e82f88bfa5998bf1b3cc5706bb6a48
100Gyeon <bjy9966@naver.com>
2023-09-28 00:40:48 +0900

    feat: 피드 개수 보여주는 부분 추가

--- src/components/page/meetingDetail/Feed/FeedPanel.tsx
+++ src/components/page/meetingDetail/Feed/FeedPanel.tsx
@@ -7,6 +7,9 @@ import { useInfinitePosts } from '@api/post/hooks';
 import FeedItem from './FeedItem';
 import { useIntersectionObserver } from '@hooks/useIntersectionObserver';
 import { TAKE_COUNT } from '@constants/feed';
+import useModal from '@hooks/useModal';
+import FeedCreateModal from './Modal/FeedCreateModal';
+import { useDisplay } from '@hooks/useDisplay';
 
 interface FeedPanelProps {
   isMember: boolean;
@@ -15,8 +18,13 @@ interface FeedPanelProps {
 const FeedPanel = ({ isMember }: FeedPanelProps) => {
   const router = useRouter();
   const meetingId = router.query.id as string;
+  const feedCreateModal = useModal();
+  const { isTablet } = useDisplay();
   const { data: postsData, fetchNextPage, hasNextPage } = useInfinitePosts(TAKE_COUNT, Number(meetingId));
   const isEmpty = !postsData?.pages[0];
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const count = postsData?.total;
   const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
     if (isIntersecting && hasNextPage) {
       fetchNextPage();
@@ -32,12 +40,28 @@ const FeedPanel = ({ isMember }: FeedPanelProps) => {
         </SContainer>
       )}
 
+      {count && (
+        <SHeader>
+          <p>
+            🔥 지금까지 쌓인 피드 <SCount>{count}</SCount>개
+          </p>
+          {isMember && (
+            <SButton onClick={feedCreateModal.handleModalOpen}>{isTablet ? '+ 작성' : '나도 작성하기'}</SButton>
+          )}
+        </SHeader>
+      )}
+
       <SFeedContainer>
         {postsData?.pages.map(post => {
           if (post) return <FeedItem key={post.id} {...post} />;
         })}
       </SFeedContainer>
       <div ref={setTarget} />
+
+      <FeedCreateModal
+        isModalOpened={feedCreateModal.isModalOpened}
+        handleModalClose={feedCreateModal.handleModalClose}
+      />
     </>
   );
 };
@@ -58,12 +82,49 @@ const SFeedContainer = styled(Box, {
   display: 'grid',
   gap: '30px',
   gridTemplateColumns: '1fr 1fr 1fr',
-  marginTop: '$56',
+  marginTop: '$32',
   width: '100%',
 
   '@tablet': {
     display: 'flex',
     flexDirection: 'column',
     marginTop: 0,
+  },
+});
+
+const SHeader = styled('div', {
+  flexType: 'center',
+  padding: '$31 0',
+  fontStyle: 'H1',
+
+  '@tablet': {
+    padding: '$16 $20',
+    fontStyle: 'H5',
+    backgroundColor: '$black80',
+    mt: '$28',
+    borderRadius: '12px',
+    flexType: 'verticalCenter',
+    justifyContent: 'space-between',
+  },
+});
+
+const SCount = styled('span', {
+  color: '$orange100',
+});
+
+const SButton = styled('button', {
+  backgroundColor: '$black40',
+  fontStyle: 'H2',
+  ml: '$48',
+  color: '$white',
+  padding: '$16 $36',
+  borderRadius: '14px',
+
+  '@tablet': {
+    backgroundColor: '$orange100',
+    fontStyle: 'T5',
+    ml: '$20',
+    padding: '$6 $12',
+    borderRadius: '8px',
   },
 });
