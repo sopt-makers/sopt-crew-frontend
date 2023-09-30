ad087e1c2daa1aeaf949f7db28753e603f137180
eunsukimme <eunsu.dev@gmail.com>
2023-10-01 01:59:43 +0900

    chore: edit playgroundUrl based on APP_ENV

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,6 @@
-export const playgroundURL = `https://playground.sopt.org`;
+export const playgroundURL =
+  process.env.NEXT_PUBLIC_APP_ENV === 'production'
+    ? `https://playground.sopt.org`
+    : 'https://sopt-internal-dev.pages.dev';
 
 export const imageS3Bucket = 'https://makers-web-img.s3.ap-northeast-2.amazonaws.com/';
