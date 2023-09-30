90f3c980b2e75aeb43b18aba1c2a30e248cfff69
eunsukimme <eunsu.dev@gmail.com>
2023-10-01 02:09:58 +0900

    fix: fix bug displaying time after now when submitted a comment

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,7 +1,18 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import 'dayjs/locale/ko';
+const localeList = dayjs.Ls;
+
 dayjs.extend(relativeTime);
 dayjs.locale('ko');
+dayjs.extend(updateLocale);
+dayjs.updateLocale('ko', {
+  relativeTime: {
+    ...localeList['ko'].relativeTime,
+    s: '방금',
+  },
+});
 
-export const fromNow = (date: string) => dayjs(date).fromNow();
+// NOTE: 댓글 작성 후 간헐적으로 작성 시간이 현재 시간보다 이후 시간으로 표기되는 이슈가 있어서 1초를 뺴준다.
+export const fromNow = (date: string) => dayjs(date).subtract(1, 's').fromNow();
