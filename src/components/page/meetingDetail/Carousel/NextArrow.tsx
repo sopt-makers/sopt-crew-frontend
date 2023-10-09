5dc7bd9f11c236f80f5d9361b008b2a8f4a70753
100Gyeon <bjy9966@naver.com>
2023-10-09 23:26:04 +0900

    feat: 모임 상세 뷰 캐로셀 내 애로우 클릭

--- src/components/page/meetingDetail/Carousel/NextArrow.tsx
+++ src/components/page/meetingDetail/Carousel/NextArrow.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { styled } from 'stitches.config';
 import ArrowBigLeftIcon from '@assets/svg/arrow_big_left.svg';
+import { ampli } from '@/ampli';
 
 interface NextArrowProps {
   className: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  total: number;
 }
 
-const NextArrow = ({ className, onClick }: NextArrowProps) => {
+const NextArrow = ({ className, onClick, total }: NextArrowProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    ampli.clickCarouselArrow({ image_total: total });
+    onClick?.(e);
+  };
+
   return (
-    <SButton className={className} onClick={onClick}>
+    <SButton className={className} onClick={e => handleClick(e)}>
       <ArrowBigLeftIcon />
     </SButton>
   );
