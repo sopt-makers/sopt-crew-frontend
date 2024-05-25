4efb976304d67ad723bcf345c419dac5181d72b7
Hyeonsu Kim <kimhyunsu11@naver.com>
2024-05-25 20:42:37 +0900

    fix: Kakao 객체 타입 수정 (후에 리팩터링 필요)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,10 +1,3 @@
-interface Kakao {
-  init: (apiKey: string) => void;
-  Channel: {
-    chat: (options: { channelPublicId: string }) => void;
-  };
-}
-
 interface Window {
-  Kakao?: Kakao;
+  Kakao: any;
 }
