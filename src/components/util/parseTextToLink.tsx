e1d9da348f0fe81b78e901dd13460c2083656fd0
Na Rim <skfla07@naver.com>
2024-07-05 00:53:51 +1000

    feat: 작성 댓글에 대한 멘션 부분 정규식 파싱 + 스타일링 및 href적용

--- src/components/util/parseTextToLink.tsx
+++ src/components/util/parseTextToLink.tsx
@@ -1,7 +1,10 @@
+import { colors } from '@sopt-makers/colors';
+
 export const parseTextToLink = (content: string) => {
   // eslint-disable-next-line no-useless-escape
   const urlRegex = /(https?:\/\/[^\s\]\)]+)|(www\.[^\s\]\)]+)/g;
   const fragmentList = content.split(urlRegex);
+
   return fragmentList.map((fragment, index) => {
     if (urlRegex.test(fragment)) {
       const url = fragment.startsWith('https') ? fragment : `https://${fragment}`;
@@ -11,6 +14,39 @@ export const parseTextToLink = (content: string) => {
         </a>
       );
     }
-    return fragment;
+
+    const processString = () => {
+      const regex = /-~!@#@(.*?)\[(\d+)\]%\^&\*\+/g;
+
+      let matches;
+      let lastIndex = 0;
+      const content = [];
+
+      // URL에서 호스트 부분 추출
+      const host = window.location.origin;
+
+      while ((matches = regex.exec(fragment)) !== null) {
+        if (matches.index > lastIndex) {
+          content.push(fragment.substring(lastIndex, matches.index));
+        }
+
+        content.push(
+          <a href={host + '/members/' + matches[2]}>
+            <p style={{ color: colors.success, display: 'inline' }}>@{matches[1]}</p>
+          </a>
+        );
+        lastIndex = regex.lastIndex;
+      }
+
+      if (lastIndex < fragment.length) {
+        content.push(fragment.substring(lastIndex));
+      }
+
+      return content;
+    };
+
+    const finalContent = processString();
+
+    return finalContent;
   });
 };
