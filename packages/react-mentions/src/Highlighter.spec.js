a566ced1b83380d5ecc1a4d3d88d8b1dac6e9d99
Hyeonsu Kim <86764406+borimong@users.noreply.github.com>
2024-07-18 23:25:48 +0900

    react-mentions 모노레포로 만들어 커스텀하기 (#790)
    * feat: react-mentions 모노레포로 만들어 커스텀하기
    
    * fix: build 에러 수정

---
+++ packages/react-mentions/src/Highlighter.spec.js
@@ -0,0 +1,7 @@
+describe('Highlighter', () => {
+  it.todo('should notify about the current caret position when mounted.')
+  it.todo('should notify about the current care position whenever it changes.')
+  it.todo('should render the current matched mentions.')
+  it.todo('should only show the matched mentions.')
+  it.todo('should be possible to style the mentions.')
+})
