dbc9dad1cc7981292337d1dd87d277462e3ef1b8
Hyeonsu Kim <kimhyunsu11@naver.com>
2024-09-07 00:24:43 +0900

    upgrade: 공지사항 api 업그레이드

--- src/api/notice/index.ts
+++ src/api/notice/index.ts
@@ -9,6 +9,6 @@ type GetNoticesResponse = {
   createdDate: string;
 }[];
 export const getNotices = async (status = 'EXPOSING') => {
-  const { data } = await api.get<Data<GetNoticesResponse>>(`/notice/v1`, { params: { status } });
+  const { data } = await api.get<Data<GetNoticesResponse>>(`/notice/v2`, { params: { status } });
   return data;
 };
