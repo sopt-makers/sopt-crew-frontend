f89ac1ff68714a60ed7ec98d8146b1b639576694
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-09-22 04:05:13 +0900

    docs: 불필요한 코드 삭제

--- src/components/contentBlocker/ContentBlocker.tsx
+++ src/components/contentBlocker/ContentBlocker.tsx
@@ -2,13 +2,9 @@ import { styled } from 'stitches.config';
 
 const ContentBlocker = () => {
   return (
-    <>
-      {/* <Divider /> */}
-      <Container>
-        <BlockerMessage>차단된 멤버의 게시글입니다.</BlockerMessage>
-      </Container>
-      {/* <Divider /> */}
-    </>
+    <Container>
+      <BlockerMessage>차단된 멤버의 게시글입니다.</BlockerMessage>
+    </Container>
   );
 };
 
@@ -38,9 +34,4 @@ const Container = styled('div', {
 const BlockerMessage = styled('p', {
   fontAg: '14_semibold_100',
   color: '$gray500',
-});
-
-const Divider = styled('hr', {
-  width: '100%',
-  border: '6px solid $gray800',
 });
