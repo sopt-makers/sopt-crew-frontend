01ab1de38b8a43633078ee7a0f41f334e3337ae1
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-10-01 17:11:26 +0900

    모임 상세 조회 API 마이그레이션 (#897)
    * refactor: 모임 상세 조회 API 마이그레이션
    
    * Delete src/api/API_LEGACY/meeting/index.ts
    
    * fix: prettier 오류 해결
    
    * fix: Data 구조 분해 및 타입 지정
    
    * fix: ESLint 오류 해결
    
    * refactor: 데이터 구조 변경

--- src/components/feed/Modal/FeedFormPresentation.tsx
+++ src/components/feed/Modal/FeedFormPresentation.tsx
@@ -119,9 +119,7 @@ function FeedFormPresentation({
 
   const uploadFile = async (file: File) => {
     const extension = file.type.split('/')[1];
-    const {
-      data: { url, fields },
-    } = await getPresignedUrl(extension);
+    const { url, fields } = await getPresignedUrl(extension);
     await uploadImage(file, url, fields);
     const imageUrls = imageS3Bucket + fields.key;
     return imageUrls;
