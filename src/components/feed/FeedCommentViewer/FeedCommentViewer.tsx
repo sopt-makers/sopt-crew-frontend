67adee02468e94e4959ac64877efd2908b1dbd14
ocahs9 <piorag99@gmail.com>
2024-10-03 23:59:53 +0900

    fix: 대댓글 케밥 아이콘 위치, 모양 이상한 것 수정(원상복구)

--- src/components/feed/FeedCommentViewer/FeedCommentViewer.tsx
+++ src/components/feed/FeedCommentViewer/FeedCommentViewer.tsx
@@ -68,7 +68,9 @@ export default function FeedCommentViewer({
           </Menu.Button>
           <MenuItems>
             {Actions.map((Action, index) => (
-              <Menu.Item key={index}>{Action}</Menu.Item>
+              <SMenuItemContainer>
+                <Menu.Item key={index}>{Action}</Menu.Item>
+              </SMenuItemContainer>
             ))}
           </MenuItems>
         </Menu>
@@ -132,8 +134,31 @@ const Date = styled('span', {
 });
 const MenuItems = styled(Menu.Items, {
   position: 'absolute',
-  top: 0,
-  right: '100%', // TODO: design 체크 필요
+  top: '35px', // TODO: design 체크 필요
+  right: '0', // TODO: design 체크 필요
+  padding: '8px',
+  borderRadius: '13px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '6px',
+  background: '$gray800',
+});
+const SMenuItemContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '80px',
+  background: '$gray800',
+
+  '&:hover': {
+    background: '$gray700',
+    borderRadius: '$8',
+  },
+
+  '&:active': {
+    background: '$gray600',
+    borderRadius: '$8',
+  },
 });
 const CommentBody = styled('div', {
   paddingLeft: '40px',
