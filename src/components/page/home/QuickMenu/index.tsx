ffc6814877f7e98430c875ea834d01efbb7708c7
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-01-01 03:44:50 +0900

    feat: Quick Menu 링크 연결

--- src/components/page/home/QuickMenu/index.tsx
+++ src/components/page/home/QuickMenu/index.tsx
@@ -3,23 +3,26 @@ import BoltIcon from '@assets/svg/button_bolt.svg?rect';
 import StudyIcon from '@assets/svg/button_study.svg?rect';
 import SeminarIcon from '@assets/svg/button_seminar.svg?rect';
 import EventIcon from '@assets/svg/button_event.svg?rect';
+import Link from 'next/link';
 
 const QuickMenu = () => {
   const menu = [
-    { Icon: SBoltIcon, label: '번쩍 모임' },
-    { Icon: SStudyIcon, label: '스터디' },
-    { Icon: SSeminarIcon, label: '세미나' },
-    { Icon: SEventIcon, label: '행사' },
+    { Icon: SBoltIcon, label: '번쩍 모임', link: '/list?category=번쩍' },
+    { Icon: SStudyIcon, label: '스터디', link: '/list?category=스터디' },
+    { Icon: SSeminarIcon, label: '세미나', link: '/list?category=세미나' },
+    { Icon: SEventIcon, label: '행사', link: '/list?category=행사' },
   ];
 
   return (
     <SContainer>
       <SGuideBtn>모임 신청 가이드</SGuideBtn>
-      {menu.map((item, idx) => (
-        <SItem key={idx}>
-          <item.Icon />
-          <SItemLabel>{item.label}</SItemLabel>
-        </SItem>
+      {menu.map(item => (
+        <Link key={item.label} href={item.link}>
+          <SItem>
+            <item.Icon />
+            <SItemLabel>{item.label}</SItemLabel>
+          </SItem>
+        </Link>
       ))}
     </SContainer>
   );
