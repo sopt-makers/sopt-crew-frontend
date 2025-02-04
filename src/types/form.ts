cee33ca6a8da1cf6da0a47ea965d6d118888cadb
Hyeonsu Kim <kimhyunsu11@naver.com>
2025-02-04 23:10:09 +0900

    chore: 후에 undefined 값으로 바뀔 수도 있음을 대비하여 optional() 추가

--- src/types/form.ts
+++ src/types/form.ts
@@ -129,7 +129,7 @@ export const flashSchema = z.object({
         label: z.string(),
         value: z.string(),
       }),
-      placeDetail: z.string().nullable(),
+      placeDetail: z.string().nullable().optional(),
     })
     .refine(data => {
       if (data.place.label === '오프라인' || data.place.label === '온라인') {
