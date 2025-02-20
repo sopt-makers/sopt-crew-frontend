a9d874f6270b3bb956e29567cc77510e6e486a72
ocahs9 <piorag99@gmail.com>
2025-02-20 19:16:38 +0900

    design: 드롭다운 필터 width 고정

--- src/components/page/list/Filter/DropDown/index.tsx
+++ src/components/page/list/Filter/DropDown/index.tsx
@@ -15,6 +15,8 @@ interface DropDownFilterProps {
 //notice: 현재 클래스 제대로 적용안되는 문제점으로 인해 !important 사용 필요
 const autoClass = css({
   width: 'auto !important',
+  minWidth: '100px',
+  whiteSpace: 'nowrap',
 });
 
 function DropDownFilter({ filter }: DropDownFilterProps) {
