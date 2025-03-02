7e239bf512ea413ae75d1c84765661ba51fe27f2
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-03-03 00:51:36 +0900

    fix: data 구조분해

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -2,10 +2,8 @@ import { paths } from '@/__generated__/schema2';
 import { api } from '@api/index';
 
 export const getCrewToken = async (playgroundToken: string) => {
-  return await api.post<paths['/auth/v2']['post']['responses']['201']['content']['application/json;charset=UTF-8']>(
-    '/auth/v2',
-    {
-      authToken: playgroundToken,
-    }
-  );
+  const response = await api.post<
+    paths['/auth/v2']['post']['responses']['201']['content']['application/json;charset=UTF-8']
+  >('/auth/v2', { authToken: playgroundToken });
+  return response.data;
 };
