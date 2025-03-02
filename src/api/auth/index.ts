7770b7d41e8c513ad57b2a968b4b9c267a6ebacf
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-03-03 01:10:27 +0900

    feat: header 제거 (#1036)

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,8 +1,8 @@
 import { paths } from '@/__generated__/schema2';
-import { api } from '@api/index';
+import { baseApi } from '@api/index';
 
 export const getCrewToken = async (playgroundToken: string) => {
-  const response = await api.post<
+  const response = await baseApi.post<
     paths['/auth/v2']['post']['responses']['201']['content']['application/json;charset=UTF-8']
   >('/auth/v2', { authToken: playgroundToken });
   return response.data;
