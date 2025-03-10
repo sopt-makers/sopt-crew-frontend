8c56152e16fbf336428940969f8355c9404a7a7f
minjeoong <orange_mj@naver.com>
2025-03-10 22:58:30 +0900

    refector: 공통 컴포넌트 추출

---
+++ src/components/feed/FeedActionsContainer/index.tsx
@@ -0,0 +1,82 @@
+import FeedActionButton from '@components/feed/FeedActionButton/FeedActionButton';
+import FeedEditModal from '@components/feed/Modal/FeedEditModal';
+import ConfirmModal from '@components/modal/ConfirmModal';
+import ReWriteIcon from '@assets/svg/comment-write.svg';
+import TrashIcon from '@assets/svg/trash.svg';
+import AlertIcon from '@assets/svg/alert-triangle.svg';
+import { CreateOverlayElement } from '@hooks/useOverlay/types';
+
+interface FeedActionsProps {
+  postId: number;
+  isMine: boolean;
+  handleDelete: () => void;
+  handleReport: () => void;
+  overlay: {
+    open: (overlayElement: CreateOverlayElement) => void;
+    close: () => void;
+  };
+}
+
+const FeedActionsContainer = ({
+  postId,
+  isMine,
+  handleDelete,
+  handleReport,
+  overlay,
+}: FeedActionsProps): React.ReactNode[] => {
+  if (isMine) {
+    return [
+      <FeedActionButton
+        onClick={() =>
+          overlay.open(({ isOpen, close }) => (
+            <FeedEditModal isModalOpened={isOpen} postId={String(postId)} handleModalClose={close} />
+          ))
+        }
+      >
+        <ReWriteIcon />
+        수정
+      </FeedActionButton>,
+      <FeedActionButton
+        onClick={() => {
+          overlay.open(({ isOpen, close }) => (
+            // eslint-disable-next-line prettier/prettier
+            <ConfirmModal
+              isModalOpened={isOpen}
+              message="게시글을 삭제하시겠습니까?"
+              cancelButton="돌아가기"
+              confirmButton="삭제하기"
+              handleModalClose={close}
+              handleConfirm={handleDelete}
+            />
+          ));
+        }}
+      >
+        <TrashIcon />
+        삭제
+      </FeedActionButton>,
+    ];
+  }
+
+  return [
+    <FeedActionButton
+      onClick={() => {
+        overlay.open(({ isOpen, close }) => (
+          // eslint-disable-next-line prettier/prettier
+          <ConfirmModal
+            isModalOpened={isOpen}
+            message="게시글을 신고하시겠습니까?"
+            cancelButton="돌아가기"
+            confirmButton="신고하기"
+            handleModalClose={close}
+            handleConfirm={handleReport}
+          />
+        ));
+      }}
+    >
+      <AlertIcon />
+      신고
+    </FeedActionButton>,
+  ];
+};
+
+export default FeedActionsContainer;
