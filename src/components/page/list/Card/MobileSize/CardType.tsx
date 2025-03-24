04def34bb8b33882d530262d79f5776b5c77c351
Hyeonsu Kim <kimhyunsu11@naver.com>
2025-03-24 18:58:08 +0900

    feat: 이미지 리사이저 시범 적용

--- src/components/page/list/Card/MobileSize/CardType.tsx
+++ src/components/page/list/Card/MobileSize/CardType.tsx
@@ -1,6 +1,7 @@
 import { RECRUITMENT_STATUS } from '@constants/option';
 import { styled } from 'stitches.config';
 import { MobileSizeCardProps } from '.';
+import { getResizedImage } from '@utils/image';
 
 function CardType({ meetingData }: Pick<MobileSizeCardProps, 'meetingData'>) {
   return (
@@ -9,7 +10,7 @@ function CardType({ meetingData }: Pick<MobileSizeCardProps, 'meetingData'>) {
         <SStatus recruitingStatus={meetingData.status}>{RECRUITMENT_STATUS[meetingData.status]}</SStatus>
         <SThumbnailImage
           css={{
-            backgroundImage: `url(${meetingData.imageURL[0]?.url})`,
+            backgroundImage: `url(${getResizedImage(meetingData.imageURL[0]?.url ?? '', 162)})`,
             backgroundSize: 'cover',
           }}
         />
