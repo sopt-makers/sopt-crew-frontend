b22c97b9f7351dc162a5d54dbf28092e630feb8b
j-nary <dlwls0921@naver.com>
2025-05-16 04:06:49 +0900

    refactor: click 이벤트 핸들러 render 밖으로 분리

--- src/components/form/Presentation/KeywordField/index.tsx
+++ src/components/form/Presentation/KeywordField/index.tsx
@@ -4,11 +4,27 @@ import Label from '@components/form/Label';
 import { Option } from '@components/form/Select/OptionItem';
 import { keywordOptions } from '@data/options';
 import { Chip } from '@sopt-makers/ui';
+import { useCallback } from 'react';
 import { styled } from 'stitches.config';
 
 const MAX_KEYWORD_COUNT = 2;
 
 const KeywordField = () => {
+  const handleClick = useCallback((option: Option, value: string[], onChange: (value: string[]) => void) => {
+    if (!option.value) return;
+
+    let updatedKeywords = [...value];
+    if (updatedKeywords.includes(option.value)) {
+      updatedKeywords = updatedKeywords.filter(keyword => keyword !== option.value);
+    } else {
+      updatedKeywords.push(option.value);
+    }
+
+    if (updatedKeywords.length > MAX_KEYWORD_COUNT) return;
+
+    onChange(updatedKeywords);
+  }, []);
+
   return (
     <div>
       <Label required={true}>모임 키워드</Label>
@@ -17,19 +33,6 @@ const KeywordField = () => {
         name="meetingKeywordTypes"
         defaultValue={[]}
         render={({ field: { value, onChange } }) => {
-          const handleClick = (option: Option) => {
-            let updatedKeywords = [...value];
-            if (updatedKeywords.includes(option.value)) {
-              updatedKeywords = updatedKeywords.filter(keyword => keyword !== option.value);
-            } else {
-              updatedKeywords.push(option.value);
-            }
-
-            if (updatedKeywords.length > MAX_KEYWORD_COUNT) return;
-
-            onChange(updatedKeywords);
-          };
-
           return (
             <SChipContainer>
               {keywordOptions.map(option => {
@@ -39,7 +42,7 @@ const KeywordField = () => {
                     disabled={value.length >= MAX_KEYWORD_COUNT && !isSelected}
                     key={option.value}
                     active={isSelected}
-                    onClick={() => handleClick(option)}
+                    onClick={() => handleClick(option, value, onChange)}
                   >
                     {option.label}
                   </Chip>
