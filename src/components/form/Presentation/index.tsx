cd9a904459289709f9750d72b9e96b9e631412ab
j-nary <dlwls0921@naver.com>
2025-05-16 02:55:33 +0900

    refactor: DescriptionField 분리

--- src/components/form/Presentation/index.tsx
+++ src/components/form/Presentation/index.tsx
@@ -25,7 +25,7 @@ import Label from '../Label';
 import Textarea from '../Textarea';
 import TextInput from '../TextInput';
 import CoLeader from './CoLeader';
-
+import DescriptionField from './DescriptionField';
 interface PresentationProps {
   submitButtonLabel: React.ReactNode;
   cancelButtonLabel?: React.ReactNode;
@@ -133,25 +133,9 @@ function Presentation({
           <CategoryField />
           <KeywordField />
           <ImageField onChangeFile={handleChangeFile} onDeleteFile={handleDeleteFile} onAddFiles={handleAddFiles} />
-
-          {/* 모임 소개 */}
-          <div>
-            <Label required={true}>모임 소개</Label>
-            <FormController
-              name="detail.desc"
-              render={({ field, fieldState: { error } }) => (
-                <Textarea
-                  placeholder={`ex.\n• 모임 성격\n• 모임 개설 배경/목적\n• 모임의 효능`}
-                  maxLength={1000}
-                  error={error?.message}
-                  {...field}
-                />
-              )}
-            ></FormController>
-          </div>
+          <DescriptionField />
         </div>
       </div>
-      {/* 모임 정보 끝 */}
       <div>
         <SFormSectionDivider>2. 활동 정보</SFormSectionDivider>
         <SectionLine />
