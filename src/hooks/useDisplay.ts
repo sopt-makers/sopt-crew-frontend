7780d0753691f1a5e85033b39dc48728cb11987d
minjeoong <orange_mj@naver.com>
2025-05-28 01:28:32 +0900

    feat: useDisplay set

--- src/hooks/useDisplay.ts
+++ src/hooks/useDisplay.ts
@@ -4,19 +4,19 @@ import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
 export function useDisplay() {
   const [isMobile, setIsMobile] = useState(false);
-  const [isTablet, setIsTable] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
   const [isLaptop, setIsLaptop] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
-  const mobile = useMediaQuery({ query: '(max-width: 430px)' });
-  const tablet = useMediaQuery({ query: '(max-width: 840px)' });
+  const mobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const tablet = useMediaQuery({ query: '(max-width: 1023px)' });
   const laptop = useMediaQuery({ query: '(max-width: 1259px)' });
-  const desktop = useMediaQuery({ query: '(min-width: 768px)' });
+  const desktop = useMediaQuery({ query: '(min-width: 1024px)' }); // default is desktop
 
   useIsomorphicLayoutEffect(() => {
     setIsMobile(mobile);
   }, [mobile]);
   useIsomorphicLayoutEffect(() => {
-    setIsTable(tablet);
+    setIsTablet(tablet);
   }, [tablet]);
   useIsomorphicLayoutEffect(() => {
     setIsLaptop(laptop);
