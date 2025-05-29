d701b99f84a8a949df2fded6b7f56a81880339d0
Lee jin (nari) <83453646+j-nary@users.noreply.github.com>
2025-05-30 02:32:42 +0900

    Editable Contents API 연결 (#1136)
    * feat: API 추가
    
    * feat: API 연결
    
    * feat: property key 상수화
    
    * refactor: API에서 key 접근하도록 수정

--- src/components/page/home/HomeCardList/CardList.tsx
+++ src/components/page/home/HomeCardList/CardList.tsx
@@ -1,4 +1,4 @@
-import { GroupBrowsingCardResponse } from '@api/API_LEGACY/meeting';
+import { useGetRecommendMeetingListQuery } from '@api/meeting/hook';
 import DesktopCard from '@components/page/home/HomeCardList/DesktopCard';
 import MobileCard from '@components/page/home/HomeCardList/MobileCard';
 import { useDisplay } from '@hooks/useDisplay';
@@ -9,12 +9,14 @@ type HomeCardProps = {
   label: string;
   isMore?: boolean;
   onMoreClick?: () => void;
-  data: GroupBrowsingCardResponse;
+  meetingIds: number[];
 };
 
-const CardList = ({ label, isMore = false, onMoreClick = () => {}, data }: HomeCardProps) => {
+const CardList = ({ label, isMore = false, onMoreClick = () => {}, meetingIds }: HomeCardProps) => {
   const { isTablet } = useDisplay();
+  const { data } = useGetRecommendMeetingListQuery({ meetingIds });
 
+  if (!data) return null;
   return (
     <SCardListWrapper>
       <STitleWrapper>
