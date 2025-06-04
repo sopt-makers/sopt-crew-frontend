4da6e62804906fd32b764f8d7f600d24a8aa40fe
minjeoong <orange_mj@naver.com>
2025-06-04 23:49:56 +0900

    feat: 툴팁 적용

--- src/components/GuideButton/index.tsx
+++ src/components/GuideButton/index.tsx
@@ -1,46 +1,48 @@
 import { styled } from 'stitches.config';
-import ArrowRightCircleIcon from '@assets/svg/arrow_right_circle.svg';
+import { Tooltip } from '@components/ToolTip/Tooltip';
+import { useState } from 'react';
+import { Tag } from '@sopt-makers/ui';
+import { useDisplay } from '@hooks/useDisplay';
+import { IconBell, IconChevronRight } from '@sopt-makers/icons';
 
 const GuideButton = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const { isLaptop } = useDisplay();
   return (
-    <SGuideButton
-      target="_blank"
-      href="https://www.notion.so/sopt-makers/eec46a4562ec48f0b0220153bb6ea68e"
-      rel="noreferrer noopener"
-    >
-      모임 신청 가이드
-      <ArrowRightCircleIcon />
-    </SGuideButton>
+    <Tooltip.Root isTooltipOpen={isOpen} onTooltipToggle={setIsOpen}>
+      <Tooltip.Trigger>
+        {isLaptop ? (
+          <SSettingButton>
+            키워드 알림 설정
+            <IconBell style={{ width: '20px', height: '20px' }} />
+          </SSettingButton>
+        ) : (
+          <SSettingButton>
+            <IconBell style={{ width: '20px', height: '20px' }} />
+            키워드 알림 설정
+            <IconChevronRight style={{ width: '20px', height: '20px' }} />
+          </SSettingButton>
+        )}
+      </Tooltip.Trigger>
+      <Tooltip.Content
+        TooltipClose={<Tooltip.Close />}
+        title={'키워드 알림'}
+        titleRightIcon={
+          <Tag variant="primary" size="sm">
+            NEW
+          </Tag>
+        }
+      >
+        관심 키워드 설정하고 <br />
+        신규 모임 개설 알림을 받아보세요
+      </Tooltip.Content>
+    </Tooltip.Root>
   );
-
-  // // @TODO 다음 PR 에서 바로 적용 예정
-  // const [isOpen, setIsOpen] = useState(true);
-  // return (
-  //   <Tooltip.Root isTooltipOpen={isOpen} onTooltipToggle={setIsOpen}>
-  //     <Tooltip.Trigger>
-  //       <SGuideButton>
-  //         모임 신청 가이드
-  //         <ArrowRightCircleIcon />
-  //       </SGuideButton>
-  //     </Tooltip.Trigger>
-  //     <Tooltip.Content
-  //       ToolTipClose={<Tooltip.Close icon={<IconXClose />} />}
-  //       title={'키워드 알림'}
-  //       titleRightIcon={
-  //         <Tag variant="primary" size="sm">
-  //           NEW
-  //         </Tag>
-  //       }
-  //     >
-  //       이거 툴팁이다잉
-  //     </Tooltip.Content>
-  //   </Tooltip.Root>
-  // );
 };
 
 export default GuideButton;
 
-const SGuideButton = styled('a', {
+const SSettingButton = styled('a', {
   flexType: 'verticalCenter',
   gap: '$8',
   color: '$gray10',
