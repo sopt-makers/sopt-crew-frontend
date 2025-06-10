09256ed63f72cea627b36110cb20fefce01d4938
minjeoong <orange_mj@naver.com>
2025-06-11 02:31:29 +0900

    feat: 반응형 분기 -> 컴포넌트 분리

--- src/components/KeywordsSettingButton/index.tsx
+++ src/components/KeywordsSettingButton/index.tsx
@@ -1,28 +1,22 @@
-import { styled } from 'stitches.config';
-import { useEffect, useState } from 'react';
-import { Tooltip } from '@components/Tooltip/Tooltip';
-
-import { Tag } from '@sopt-makers/ui';
-import { useDisplay } from '@hooks/useDisplay';
-import { IconBell, IconChevronRight } from '@sopt-makers/icons';
+import { KeywordSettingOptionType } from '@api/user';
+import { useMutationInterestedKeywords, useQueryGetInterestedKeywords } from '@api/user/hooks';
 import AlarmSettingBottomSheet from '@components/page/list/Alarm/BottomSheet/AlarmSettingBottomSheet';
 import AlarmSettingModal from '@components/page/list/Alarm/Modal/AlarmSettingModal';
-import { useMutationInterestedKeywords, useQueryGetInterestedKeywords } from '@api/user/hooks';
-import { fontsObject } from '@sopt-makers/fonts';
-import { KeywordSettingOptionType } from '@api/user';
+import { Tooltip } from '@components/Tooltip/Tooltip';
+import { useDisplay } from '@hooks/useDisplay';
+import { useEffect, useState } from 'react';
+import { TooltipContent } from '@components/page/list/AlarmSetting/TooltipContent/TooltipContent';
+import SettingButton from '@components/page/list/AlarmSetting/SettingButton/SettingButton';
 
 const KeywordsSettingButton = () => {
   const [isTooltipOpen, setIsTooltipOpen] = useState(true);
-  const { isDesktop, isTablet, isMobile } = useDisplay();
+  const { isDesktop } = useDisplay();
 
   const { mutate: mutateUserInterested } = useMutationInterestedKeywords();
   const { data: userInterested } = useQueryGetInterestedKeywords();
 
   const [selectedAlarm, setSelectedAlarm] = useState<KeywordSettingOptionType[]>(() => userInterested?.keywords ?? []);
   const [isSettingOpen, setIsSettingOpen] = useState(false);
-
-  const isModalOpened = isSettingOpen && isDesktop;
-  const isBottomSheetOpen = isSettingOpen && !isDesktop;
 
   const handleKeywordClick = (value: KeywordSettingOptionType) => {
     setSelectedAlarm(prev => {
@@ -37,10 +31,6 @@ const KeywordsSettingButton = () => {
     mutateUserInterested([]);
   };
 
-  const handleSettingClick = () => {
-    setIsSettingOpen(true);
-  };
-
   useEffect(() => {
     // 데이터가 로드 된 시점에 "이미 선택된 키워드" 세팅
     if (userInterested?.keywords) {
@@ -52,102 +42,25 @@ const KeywordsSettingButton = () => {
     <>
       <Tooltip.Root isTooltipOpen={isTooltipOpen} onTooltipToggle={setIsTooltipOpen}>
         <Tooltip.Trigger>
-          {isMobile ? (
-            <SSettingButton onClick={handleSettingClick}>
-              <SIconBell />
-              키워드 알림 설정
-            </SSettingButton>
-          ) : isTablet ? (
-            <SSettingButton onClick={handleSettingClick}>
-              키워드 알림 설정
-              <SIconBell />
-            </SSettingButton>
-          ) : (
-            <SSettingButton onClick={handleSettingClick}>
-              <SIconBell />
-              키워드 알림 설정
-              {selectedAlarm.length > 0 && <SSelectedAlarm>{selectedAlarm.join(', ')}</SSelectedAlarm>}
-              <SIconChevronRight />
-            </SSettingButton>
-          )}
+          <SettingButton onClick={() => setIsSettingOpen(true)} selectedKeywords={selectedAlarm} />
         </Tooltip.Trigger>
-        {isMobile ? (
-          <Tooltip.Content>
-            관심 키워드 설정하고 <br />
-            신규 모임 개설 알림을 받아보세요
-          </Tooltip.Content>
-        ) : (
-          <Tooltip.Content
-            TooltipClose={<Tooltip.Close />}
-            title={'키워드 알림'}
-            titleRightIcon={
-              <Tag variant="primary" size="sm">
-                NEW
-              </Tag>
-            }
-          >
-            관심 키워드 설정하고 <br />
-            신규 모임 개설 알림을 받아보세요
-          </Tooltip.Content>
-        )}
+        <TooltipContent />
       </Tooltip.Root>
-      {isModalOpened && (
-        <AlarmSettingModal
-          isOpen={isSettingOpen && isDesktop}
-          close={() => setIsSettingOpen(false)}
-          selectedAlarm={selectedAlarm}
-          onKeywordClick={handleKeywordClick}
-          onReset={handleReset}
-        />
-      )}
-      {isBottomSheetOpen && (
-        <AlarmSettingBottomSheet
-          isOpen={isBottomSheetOpen}
-          close={() => setIsSettingOpen(false)}
-          selectedAlarm={selectedAlarm}
-          onKeywordClick={handleKeywordClick}
-        />
-      )}
+      <AlarmSettingModal
+        isOpen={isSettingOpen && isDesktop}
+        close={() => setIsSettingOpen(false)}
+        selectedAlarm={selectedAlarm}
+        onKeywordClick={handleKeywordClick}
+        onReset={handleReset}
+      />
+      <AlarmSettingBottomSheet
+        isOpen={isSettingOpen && !isDesktop}
+        close={() => setIsSettingOpen(false)}
+        selectedAlarm={selectedAlarm}
+        onKeywordClick={handleKeywordClick}
+      />
     </>
   );
 };
 
 export default KeywordsSettingButton;
-
-const SIconBell = styled(IconBell, {
-  width: '20px',
-  height: '20px',
-  '@mobile': {
-    width: '16px',
-    height: '16px',
-  },
-});
-
-const SIconChevronRight = styled(IconChevronRight, {
-  width: '20px',
-  height: '20px',
-});
-
-const SSelectedAlarm = styled('span', {
-  ...fontsObject.BODY_3_14_M,
-  color: '$blue400',
-});
-
-const SSettingButton = styled('a', {
-  flexType: 'verticalCenter',
-  gap: '$8',
-  color: '$gray10',
-  fontAg: '18_semibold_100',
-
-  '@tablet': {
-    padding: '0',
-    fontAg: '14_semibold_100',
-  },
-
-  path: {
-    stroke: '$gray10',
-  },
-  '@media (max-width: 320px)': {
-    display: 'none',
-  },
-});
