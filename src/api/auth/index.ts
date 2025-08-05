f151a7e78e50677e3f0a051b709dd1424507d19c
Minjeong Kim <88662427+minjeoong@users.noreply.github.com>
2025-08-06 01:24:11 +0900

    [Hotfix] auth api Token 주입 방식 변경 (#1162)
    * feat: authToken -> JWT token 방식으로 수정
    
    * fix: data -> config 로 오류 수정

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,6 +4,14 @@ import { baseApi } from '@api/index';
 export const getCrewToken = async (playgroundToken: string) => {
   const response = await baseApi.post<
     paths['/auth/v2']['post']['responses']['201']['content']['application/json;charset=UTF-8']
-  >('/auth/v2', { authToken: playgroundToken });
+  >(
+    '/auth/v2',
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${playgroundToken}`,
+      },
+    }
+  );
   return response.data;
 };
