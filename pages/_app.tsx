37ea665864b0a581af824acbc59eab5994a91169
최주용 <121484561+wuzoo@users.noreply.github.com>
2025-08-20 21:50:29 +0900

    인증중앙화 auth token refresh 로직 설정 (#1167)
    * feat: create auth axios instance with credential
    
    * feat: set auth storage value in dev mode
    
    * feat: set check, refresh token interceptor
    
    * chore: bump playground-common
    
    * feat: change redirect href to accounts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -94,9 +94,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     setAccessTokens();
-    const refreshTime = process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 9 * 60 * 60 * 1000 : 2 * 60 * 1000;
-    const intervalId = setInterval(setAccessTokens, refreshTime);
-    return () => clearInterval(intervalId);
   }, []);
 
   return (
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,15 @@ import { authToken } from '@/stores/tokenStore';
 import axios from 'axios';
 import { computed } from 'nanostores';
 import createClient from 'openapi-fetch';
+import { checkToken, refreshToken } from './interceptor';
 
 export type PromiseResponse<T> = { data: T; statusCode: number };
 export type Data<T> = PromiseResponse<T>;
 
 const baseURL =
   process.env.NEXT_PUBLIC_APP_ENV === 'production' ? 'https://crew.api.prod.sopt.org' : 'https://crew.api.dev.sopt.org';
+
+const authBaseURL = 'https://auth.api.dev.sopt.org';
 
 const playgroundBaseURL =
   process.env.NEXT_PUBLIC_APP_ENV === 'production'
@@ -19,6 +22,11 @@ export const baseApi = axios.create({ baseURL });
 
 export const api = axios.create({
   baseURL,
+});
+
+export const authApi = axios.create({
+  baseURL: authBaseURL,
+  withCredentials: true,
 });
 
 authToken.subscribe(newToken => {
@@ -39,3 +47,7 @@ export const apiV2 = computed(authToken, currentToken =>
     headers: currentToken ? { Authorization: `Bearer ${currentToken}` } : {},
   })
 );
+
+api.interceptors.request.use(checkToken, err => err);
+
+api.interceptors.response.use(res => res, refreshToken);
