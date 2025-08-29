1fbc7e3d6d4fd4bcc983ccba8e44a3ae6cc2b750
j-nary <dlwls0921@naver.com>
2025-08-29 15:50:39 +0900

    refactor: mutation 분리

--- src/api/user/mutation.ts
+++ src/api/user/mutation.ts
@@ -1,6 +1,6 @@
 import UserQueryKey from '@api/user/UserQueryKey';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { KeywordSettingOptionType, postInterestedKeywords } from '.';
+import { getUserMeetingAll, KeywordSettingOptionType, postInterestedKeywords } from '.';
 
 export const useMutationInterestedKeywords = () => {
   const queryClient = useQueryClient();
@@ -9,6 +9,17 @@ export const useMutationInterestedKeywords = () => {
     mutationFn: (keywords: KeywordSettingOptionType[]) => postInterestedKeywords(keywords),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: UserQueryKey.interestedKeywords() });
+    },
+  });
+};
+
+export const useUserMeetingListMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: getUserMeetingAll,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: UserQueryKey.meetingAll() });
     },
   });
 };
