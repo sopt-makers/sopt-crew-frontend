bda34e640b0b07909c24f2842976f0fa500dbbb5
j-nary <dlwls0921@naver.com>
2025-11-17 22:24:06 +0900

    refactor: localStorage get, set 로직 보완

--- src/store/localStorage/LocalStorage.ts
+++ src/store/localStorage/LocalStorage.ts
@@ -3,7 +3,8 @@ import LocalStorageKey from '@/store/localStorage/LocalStorageKey';
 class LocalStorage {
   static getItem(key: LocalStorageKey) {
     const item = localStorage.getItem(key);
-    if (!item) return null;
+    if (item == null) return null;
+
     try {
       return JSON.parse(item);
     } catch {
@@ -12,6 +13,12 @@ class LocalStorage {
   }
 
   static setItem<T>(key: LocalStorageKey, value: T) {
+    if (value == null) {
+      // value가 null, undefined 이면 저장하지 않고 삭제
+      LocalStorage.removeItem(key);
+      return;
+    }
+
     localStorage.setItem(key, JSON.stringify(value));
   }
 
