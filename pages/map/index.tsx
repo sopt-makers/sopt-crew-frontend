11f6be0469238537565422f35ed044daa38fd125
constantly-dev <rntqhdl10@naver.com>
2025-12-29 12:06:58 +0900

    feat: 솝맵 layout 추가

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,11 +1,37 @@
+import DropDownFilter from '@domain/list/Filter/DropDown';
+import Search from '@domain/list/Filter/Search';
+import MapList from '@domain/map/MapList';
 import CrewTab from '@shared/CrewTab';
+import { styled } from 'stitches.config';
+
+export const CATEGORY_OPTIONS = ['카페', '음식점', '기타'];
+
+export const CATEGORY_FILTER = {
+  label: '카테고리',
+  subject: 'category',
+  options: CATEGORY_OPTIONS,
+};
 
 const MapPage = () => {
   return (
     <div>
       <CrewTab />
+      <SFilterWrapper>
+        <Search />
+        <DropDownFilter filter={CATEGORY_FILTER} width={'160px'} />
+      </SFilterWrapper>
+
+      <MapList />
     </div>
   );
 };
+
+const SFilterWrapper = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  mt: '$45',
+  mb: '$30',
+});
 
 export default MapPage;
