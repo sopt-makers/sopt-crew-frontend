2166ffbb4b4c9191dd2ff52ad78e1f93960c13cf
constantly-dev <rntqhdl10@naver.com>
2026-01-02 09:52:44 +0900

    feat: map list component 구현

---
+++ src/domain/map/MapList.tsx
@@ -0,0 +1,27 @@
+import MapCard from '@domain/map/Card/index';
+import { styled } from 'stitches.config';
+
+const MapList = () => {
+  return (
+    <main>
+      <SCardWrapper>
+        {[...Array(10)].map((_, index) => (
+          <MapCard key={index} />
+        ))}
+      </SCardWrapper>
+    </main>
+  );
+};
+
+const SCardWrapper = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '12px',
+  mt: '$24',
+
+  '@mobile': {
+    mt: '$16',
+  },
+});
+
+export default MapList;
