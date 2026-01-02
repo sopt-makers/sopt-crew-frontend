f89be0d98fd8bfe17692901d0f02ae781f003293
constantly-dev <rntqhdl10@naver.com>
2026-01-02 17:31:07 +0900

    feat: 초기화 버튼 구현

--- src/domain/map/Filter/Reset/index.tsx
+++ src/domain/map/Filter/Reset/index.tsx
@@ -1,34 +1,20 @@
 import { useCategoryParams } from '@hook/queryString/custom';
-import { IconRefresh } from '@sopt-makers/icons';
+import { fontsObject } from '@sopt-makers/fonts';
 import { styled } from 'stitches.config';
 
 const FilterResetButton = () => {
-  const { resetQuery } = useCategoryParams();
+  const { resetQuery: resetCategory } = useCategoryParams();
 
   const handleFilterRefresh = () => {
-    resetQuery();
+    resetCategory();
   };
 
-  return (
-    <RefreshButton onClick={handleFilterRefresh}>
-      <IconRefresh />
-    </RefreshButton>
-  );
+  return <RefreshButton onClick={handleFilterRefresh}>초기화</RefreshButton>;
 };
 
 export default FilterResetButton;
 
-const RefreshButton = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRadius: '$8',
-  background: '$gray800',
-  padding: '$14 $16',
-  cursor: 'pointer',
-
-  '& > svg': {
-    width: '$20',
-    height: '$20',
-  },
+const RefreshButton = styled('button', {
+  ...fontsObject.LABEL_2_16_SB,
+  color: '$success',
 });
