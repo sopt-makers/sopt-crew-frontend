9e321c26b3337bbafe92890568939b055be57bfe
constantly-dev <rntqhdl10@naver.com>
2026-01-02 13:34:17 +0900

    chore: 필터링 관련 컴포넌트 filter 폴더로 이동

--- src/domain/map/OrderFilter/constant.ts
+++
@@ -1,9 +0,0 @@
-export type Option<T = string> = {
-  value: T;
-  label: string;
-};
-
-export const ORDER_OPTIONS: Option<string>[] = [
-  { label: '최신순', value: 'LATEST' },
-  { label: '인기순', value: 'POPULAR' },
-];
