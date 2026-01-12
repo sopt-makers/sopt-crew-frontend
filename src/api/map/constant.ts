68624de62edd7a51d00081fccc0e5be3cfa2bff2
constantly-dev <rntqhdl10@naver.com>
2026-01-12 22:20:22 +0900

    fix: api schema category에서 categories로 파라미터 변경되어 로직 수정

---
+++ src/api/map/constant.ts
@@ -0,0 +1,5 @@
+export const SERVER_CATEGORY_MAP: Record<string, 'FOOD' | 'CAFE' | 'ETC'> = {
+  카페: 'CAFE',
+  음식점: 'FOOD',
+  기타: 'ETC',
+};
--- src/api/map/query.ts
+++ src/api/map/query.ts
@@ -1,22 +1,32 @@
-import { useCategoryParams, usePageParams, useSortTypeParams, useStationKeywordParams } from '@hook/queryString/custom';
+import {
+  useCategoriesParams,
+  usePageParams,
+  useSortTypeParams,
+  useStationKeywordParams,
+} from '@hook/queryString/custom';
 import { queryOptions } from '@tanstack/react-query';
 import { getMapList } from '.';
+import { SERVER_CATEGORY_MAP } from './constant';
 import MapQueryKey from './MapQueryKey';
 import { GetMapList } from './type';
 
 export const useMapListQueryOption = () => {
-  const { value: category } = useCategoryParams();
+  const { value: categories } = useCategoriesParams();
   const { value: sortType } = useSortTypeParams();
   const { value: stationKeyword } = useStationKeywordParams();
   const { value: page } = usePageParams();
 
   type MapListRequest = NonNullable<GetMapList['request']>;
 
+  const convertedCategories = categories?.map(kor => SERVER_CATEGORY_MAP[kor]).filter(Boolean);
+
   const params: MapListRequest = {
     page: Number(page) || 1,
     take: 10,
     sortType: (sortType as MapListRequest['sortType']) ?? 'LATEST',
-    categories: category?.length ? (category as MapListRequest['categories']) : undefined,
+    categories: convertedCategories?.length
+      ? (convertedCategories.join(',') as unknown as MapListRequest['categories'])
+      : undefined,
     stationKeyword: stationKeyword as MapListRequest['stationKeyword'],
   };
 
