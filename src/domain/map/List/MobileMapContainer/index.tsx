2918e3bb1226057afda68cd801f2ee5d61359629
constantly-dev <rntqhdl10@naver.com>
2026-01-13 00:57:31 +0900

    feat: useInfiniteScroll 적용

--- src/domain/map/List/MobileMapContainer/index.tsx
+++ src/domain/map/List/MobileMapContainer/index.tsx
@@ -1,20 +1,13 @@
 import { useMapListInfiniteQueryOption } from '@api/map/query';
 import Filter from '@domain/map/Filter';
+import { useInfiniteScroll } from '@hook/useInfiniteScroll';
 import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
 import MapList from '../MapList';
 
 const MobileMapContainer = () => {
   const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery(useMapListInfiniteQueryOption());
 
-  const { ref, inView } = useInView();
-
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, fetchNextPage]);
+  const ref = useInfiniteScroll({ hasNextPage, fetchNextPage });
 
   const allMaps = data.pages.flatMap(page => page.soptMaps);
   const totalCount = data.pages[0]?.meta.itemCount ?? 0;
