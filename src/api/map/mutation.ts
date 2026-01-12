439d93863ca2cb0c49e61dc4f9f8604a6f401283
constantly-dev <rntqhdl10@naver.com>
2026-01-13 03:35:47 +0900

    feat: useRecommendMapMutation mutation function 구현

--- src/api/map/mutation.ts
+++ src/api/map/mutation.ts
@@ -1,6 +1,8 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { deleteMap } from '.';
+import { InfiniteData, useMutation, useQueryClient } from '@tanstack/react-query';
+import { produce } from 'immer';
+import { deleteMap, putMapRecommendation } from '.';
 import MapQueryKey from './MapQueryKey';
+import { GetMapList } from './type';
 
 export const useDeleteMapMutation = () => {
   const queryClient = useQueryClient();
@@ -9,5 +11,62 @@ export const useDeleteMapMutation = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: MapQueryKey.list() });
     },
+  });
+};
+
+export const useRecommendMapMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (mapId: number) => putMapRecommendation(mapId),
+
+    onMutate: async targetMapId => {
+      await queryClient.cancelQueries({ queryKey: MapQueryKey.all() });
+
+      const previousData = queryClient.getQueriesData({ queryKey: MapQueryKey.all() });
+
+      queryClient.setQueriesData<InfiniteData<GetMapList['response']>>(
+        { queryKey: [MapQueryKey.infiniteList()] },
+        oldData => {
+          if (!oldData) return undefined;
+
+          return produce(oldData, draft => {
+            draft.pages.forEach(page => {
+              const target = page.soptMaps.find(map => map.id === targetMapId);
+              if (target) {
+                target.isRecommended = !target.isRecommended;
+                target.recommendCount = (target.recommendCount ?? 0) + (target.isRecommended ? 1 : -1);
+              }
+            });
+          });
+        }
+      );
+
+      queryClient.setQueriesData<GetMapList['response']>({ queryKey: MapQueryKey.list() }, oldData => {
+        if (!oldData) return undefined;
+
+        return produce(oldData, draft => {
+          const target = draft.soptMaps.find(map => map.id === targetMapId);
+          if (target) {
+            target.isRecommended = !target.isRecommended;
+            target.recommendCount = (target.recommendCount ?? 0) + (target.isRecommended ? 1 : -1);
+          }
+        });
+      });
+
+      return { previousData };
+    },
+
+    onError: (err, newTodo, context) => {
+      if (context?.previousData) {
+        context.previousData.forEach(([queryKey, data]) => {
+          queryClient.setQueryData(queryKey, data);
+        });
+      }
+    },
+
+    // onSettled: () => {
+    //   queryClient.invalidateQueries({ queryKey: MapQueryKey.all() });
+    // },
   });
 };
