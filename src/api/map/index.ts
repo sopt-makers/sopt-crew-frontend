fa00a2388285a906c2990bed937b8f2b4c8d2d66
constantly-dev <rntqhdl10@naver.com>
2026-01-13 01:54:05 +0900

    feat: deleteMap api function 구현

--- src/api/map/index.ts
+++ src/api/map/index.ts
@@ -4,3 +4,8 @@ import { GetMapList } from '@api/map/type';
 export const getMapList = async (params: GetMapList['request']) => {
   return (await api.get<GetMapList['response']>('/api/v2/map', { params })).data;
 };
+
+export const deleteMap = async (mapId: number) => {
+  const { data } = await api.delete(`/api/v2/map/${mapId}`);
+  return data;
+};
