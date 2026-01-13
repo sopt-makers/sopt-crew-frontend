27566d662e428c6f73bee46571182c85c84ebd73
Yoo TaeSeung <remicon99@gmail.com>
2026-01-13 23:02:02 +0900

    fix: 한줄소개 글자수 변경

--- src/domain/map/Form/DescriptionField/index.tsx
+++ src/domain/map/Form/DescriptionField/index.tsx
@@ -11,7 +11,7 @@ const DescriptionField = () => {
         render={({ field, fieldState: { error } }) => (
           <Textarea
             placeholder={`ex.\n• 모든 자리에 콘센트가 있어요.\n• 화장실이 깨끗해요.`}
-            maxLength={1000}
+            maxLength={80}
             error={error?.message}
             {...field}
           />
