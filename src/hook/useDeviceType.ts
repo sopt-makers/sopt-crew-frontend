242ba833149868c5fddbcc970082bac96bb34e7c
constantly-dev <rntqhdl10@naver.com>
2026-01-18 00:40:39 +0900

    feat: useDeviceType hook 구현

---
+++ src/hook/useDeviceType.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export const useDeviceType = () => {
+  const [deviceInfo, setDeviceInfo] = useState({
+    isMobile: false,
+    isDesktop: false,
+  });
+
+  useEffect(() => {
+    const ua = navigator.userAgent;
+
+    const mobileRegex = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    const isMobileUserAgent = mobileRegex.test(ua);
+
+    const isIpadOS = ua.includes('Macintosh') && navigator.maxTouchPoints > 1;
+
+    const isMobile = isMobileUserAgent || isIpadOS;
+
+    setDeviceInfo({
+      isMobile,
+      isDesktop: !isMobile,
+    });
+  }, []);
+
+  return deviceInfo;
+};
