bea4491ce1a19811a33ff35881e1f037b50bda97
j-nary <dlwls0921@naver.com>
2026-01-17 22:08:44 +0900

    feat: filter, viewSoptmap 연결

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,13 +1,18 @@
+import { ampli } from '@/ampli';
 import Loader from '@common/loader/Loader';
 import DesktopMapContainer from '@domain/map/List/DesktopMapContainer';
 import MobileMapContainer from '@domain/map/List/MobileMapContainer';
 import { useDisplay } from '@hook/useDisplay';
 import CrewTab from '@shared/CrewTab';
 import FloatingButton from '@shared/FloatingButton';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 const MapPage = () => {
   const { isDesktop } = useDisplay();
+
+  useEffect(() => {
+    ampli.viewSoptmapTab();
+  }, []);
 
   return (
     <div>
