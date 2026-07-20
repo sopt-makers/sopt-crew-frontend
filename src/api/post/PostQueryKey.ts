8d65693d041174d9b570b96723ba791bd36b6cdf
TaeSeung Yoo <59465914+gudusol@users.noreply.github.com>
2026-07-20 14:11:36 +0900

    모임 상세 피드 목록 서스펜스 (#1219)
    * refactor: feedListSkeleton  리팩토링
    
    * feat: 피드 리스트 suspense 처리
    
    * feat: useGetPostListQueryOption 추가
    
    * fix: '이 모임의 다른피드' 조회 방식 변경(무한쿼리 -> 리스트 쿼리)
    
    * fix: queryKey 오류 수정

--- src/api/post/PostQueryKey.ts
+++ src/api/post/PostQueryKey.ts
@@ -1,6 +1,7 @@
 const PostQueryKey = {
   all: () => ['post'] as const,
-  list: (take: number, meetingId?: number) => [...PostQueryKey.all(), take, meetingId] as const,
+  list: (meetingId?: number) => [...PostQueryKey.all(), meetingId] as const,
+  infiniteList: (take: number, meetingId?: number) => [...PostQueryKey.all(), take, meetingId, 'infinite'] as const,
   detail: (id: number) => [...PostQueryKey.all(), id] as const,
   count: (meetingId: number) => [...PostQueryKey.list(meetingId), 'count'] as const,
 };
